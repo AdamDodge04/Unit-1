244380109992ee8bd4cf6a90688d17d8691d5a2a
AdamDodge04 <815495@mystma.org>
2020-04-08 16:27:12 +0000

    Added top five movies

---
+++ Unit1/src/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("1.Wall-E");
+        System.out.println("2.Toy Story");
+        System.out.println("3.Joker");
+        System.out.println("4.Hunger Games");
+        System.out.println("5.Avengers Endgame");
+    }
+}
