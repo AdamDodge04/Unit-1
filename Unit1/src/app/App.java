2ad454de2974c5f3a7d5c3d22b7778cedec95ab3
AdamDodge04 <815495@mystma.org>
2020-04-16 18:54:05 +0000

    For Loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,32 +2,22 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        for (int a = 22; a <= 28; a = a + 3){
+           System.out.println(a);
+        }
+        for (int b = 2; b <= 32; b = b * 2){
+            System.out.println(b);
+        }
+        for (int c = 3; c >= -81; c = c * -c){
+            System.out.println(c);
+        }
+        for (int d = 50; d >= 5; d = d -5){
+            System.out.println(d);
+        }
+        for (int e = 0; e <= 5; e = e + 1){
+            System.out.println(e * 2);
+            System.out.println(e);
+        }
 
-        for (int i = 0; i <= 10; i = i +1){
-            System.out.println("The count is " + i);
-            System.out.println(" Lets keep counting. ");
-        }
-        for (int a = 10; a <= 20; a = a +1){
-            System.out.println("The a count is " + a);
-            System.out.println(" Lets keep counting. ");
-        }
-        for (int b = 10; b <= 100; b = b +10){
-            System.out.println("The b count is " + b);
-            System.out.println(" Lets keep counting. ");
-        }
-        for (int c = 1000; c <= 256000; c = c +1000){
-            System.out.println("The c count is " + c);
-            System.out.println(" Lets keep counting. ");
-        }
-        for (int d = 11; d <= 12; d = d +1){
-            System.out.println("The d count is " + d);
-            System.out.println(" Lets keep counting. ");
-        }
-        for (int e = 10; e >= 0; e = e -1){
-            System.out.println("The e count is " + e);
-            System.out.println(" Lets keep counting. ");
-        }
-      
-      
     }
 }
