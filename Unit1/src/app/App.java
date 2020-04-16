e9919d4c5ce6f3aa845601b44fcd6b749b7af34a
AdamDodge04 <815495@mystma.org>
2020-04-16 16:05:15 +0000

    For loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,31 +2,32 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       boolean livesInAlbertville = false;
-       char firstLetterOfName = 'A';
-       byte myAgeInYears = 15;
-       Short myAgeInDays = 5830;
-       int myAgeInMinutes = 8395453;
-       long distanceFromPluto = 4670000000L;
-       float firstSevenDigitsOfPi = (float) 3.1415926;
-       double firstSixteenDigitsOfPi = 3.1415926535897932;
 
-
-       System.out.println("The first sixteen digits of pi are");
-       System.out.println(firstSixteenDigitsOfPi);
-       System.out.println("The first seven digits of pi are");
-       System.out.println(firstSevenDigitsOfPi);
-       System.out.println("The distance from pluto in miles is");
-       System.out.println(distanceFromPluto);
-       System.out.println("My age in munutes is");
-       System.out.println(myAgeInMinutes);
-       System.out.println("My age in days is");
-       System.out.println(myAgeInDays);
-       System.out.println("My age in years is");
-       System.out.println(myAgeInYears);
-       System.out.println("The first letter of my name is");
-       System.out.println(firstLetterOfName);
-       System.out.println("Do I live in Albertville");
-       System.out.println(livesInAlbertville);
+        for (int i = 0; i <= 10; i = i +1){
+            System.out.println("The count is " + i);
+            System.out.println(" Lets keep counting. ");
+        }
+        for (int a = 10; a <= 20; a = a +1){
+            System.out.println("The a count is " + a);
+            System.out.println(" Lets keep counting. ");
+        }
+        for (int b = 10; b <= 100; b = b +10){
+            System.out.println("The b count is " + b);
+            System.out.println(" Lets keep counting. ");
+        }
+        for (int c = 1000; c <= 256000; c = c +1000){
+            System.out.println("The c count is " + c);
+            System.out.println(" Lets keep counting. ");
+        }
+        for (int d = 11; d <= 12; d = d +1){
+            System.out.println("The d count is " + d);
+            System.out.println(" Lets keep counting. ");
+        }
+        for (int e = 10; e >= 0; e = e -1){
+            System.out.println("The e count is " + e);
+            System.out.println(" Lets keep counting. ");
+        }
+      
+      
     }
 }
