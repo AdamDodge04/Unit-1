623d65054bbfab70a48d3342f5f64ac7a3cd5641
AdamDodge04 <815495@mystma.org>
2020-04-21 17:46:47 +0000

    Best i can do for loops challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,14 +3,28 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for (int i = 1; i <= 20; i++) {
+        System.out.println("Start first loop");
 
-            if (i % 4 == 0) {
-                System.out.println(i + " is divisible by 4");
-            } else {
-                System.out.println(i + " cannot be divided by 4");
+        for (int i = 0; i <= 19; i++) {
+            System.out.println(i);
+            if (i == 10) {
+                System.out.println(i);
             }
         }
-        System.out.println("All finished");
+        System.out.println("End first loop");
+
+        System.out.println("Start second loop");
+        for (int i = -1; i <= 19; i = i + 2) {
+            System.out.println(i);
+            if (i % 1 == 0) {
+                System.out.println(i = i + 1);
+            }
+            else {
+                System.out.println(i = i + 1);
+            }
+
+        }
+        System.out.println("End second loop");
+
     }
 }
