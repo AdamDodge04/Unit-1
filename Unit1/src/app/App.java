ba27dbdb389acfc39020e1f90543b9cf9ef72f6a
AdamDodge04 <815495@mystma.org>
2020-04-22 17:00:03 +0000

    fixed For Loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -8,23 +8,26 @@ public class App {
         for (int i = 0; i <= 19; i++) {
             System.out.println(i);
             if (i == 10) {
-                System.out.println(i);
+                for (int j = 0; j < 5; j++) {
+                    System.out.println(i);
+                }
             }
         }
         System.out.println("End first loop");
 
         System.out.println("Start second loop");
-        for (int i = -1; i <= 19; i = i + 2) {
+        for (int i = 0; i < 20; i = i + 2){
+            if (i % 3 == 0){
+                i = i - 1;
+            }
             System.out.println(i);
-            if (i % 1 == 0) {
-                System.out.println(i = i + 1);
-            }
-            else {
-                System.out.println(i = i + 1);
-            }
-
         }
+        
+            
+
+        
         System.out.println("End second loop");
+        
 
     }
 }
