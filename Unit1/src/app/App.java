a37b90bb6f84f8a960e0873407aa3282e7ad544e
AdamDodge04 <815495@mystma.org>
2020-04-23 06:55:11 +0000

    while loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,21 +2,30 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String person1 = "Adam ";
-        String verb1 = "golf";
-        String person2 = "Jack";
-        String verb2 = "talk ";
-        String verb3 = "shop ";
-        String food = "tacos ";
-        String dayOfWeek = "Tuesday ";
-        String place1 = "home ";
-        String place2 = "game store ";
-        String adjective1 = "amazing ";
-        String adjective2 = "huge ";
+        int count = 0;
+        String color = "red";
+        boolean earthIsRound = true;
 
-        System.out.println(person1 + "is a programmer who likes to " + verb1 + ".");
-        System.out.println("Every " + dayOfWeek + "they make a " + adjective1 + food + "for breakfast and " + verb2 + "at " + place1 + "with " + person2 + ".");
-        System.out.println("Sometimes, they go to the " + adjective2 + place2 + "with " + person2 + " so that they can " + verb3 + "togehter.");
+
+        while (count <= 10){
+            System.out.println("The count is " + count);
+            count = count + 1;
+        }
+
+        System.out.println("count finished");
+
+        while (color == "red"){
+            System.out.println("The color is " + color);
+            color = "green";
+        }
+
+        System.out.println("Color finished");
+
+        while (earthIsRound == true){
+            System.out.println("The earth is round = " + earthIsRound);
+            earthIsRound = false;
+        }
+        
 
      
 
