80fa4c8fcbf8576f751dfbedc914939faa24aa89
AdamDodge04 <815495@mystma.org>
2020-04-23 16:22:48 +0000

    For if else loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,32 +2,23 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int count = 0;
-        String color = "red";
-        boolean earthIsRound = true;
-
-
-        while (count <= 10){
-            System.out.println("The count is " + count);
-            count = count + 1;
+        System.out.println("Double or single digit?");
+        for (int i = 20; i >= 0; i--) {
+            if (i >= 10) {
+                System.out.println(i + " is double digit");
+            } else {
+                System.out.println(i + " is single digit");
+            }
         }
-
-        System.out.println("count finished");
-
-        while (color == "red"){
-            System.out.println("The color is " + color);
-            color = "green";
+        System.out.println("Nice!");
+        System.out.println("Fast food");
+        for (int i = 1; i < 12; i = i + 1) {
+            if (i % 2 == 0) {
+                System.out.println("Fries");
+            } else {
+                System.out.println("Burgers");
+            }
         }
-
-        System.out.println("Color finished");
-
-        while (earthIsRound == true){
-            System.out.println("The earth is round = " + earthIsRound);
-            earthIsRound = false;
-        }
-        
-
-     
-
+        System.out.println("Nice!");
     }
 }
