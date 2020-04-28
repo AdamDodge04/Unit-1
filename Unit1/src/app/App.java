85938e1fc5bf38b9533bab4ebd9c53188acd17fc
AdamDodge04 <815495@mystma.org>
2020-04-28 17:31:51 +0000

    User Input

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,25 +1,32 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Start first loop");
-        for (int i = 1; i <= 10; i++) {
-            if (i == 5) {
-                System.out.println("five");
-                i = i + 1;
-            }
-            System.out.println(i);
-        }
-        System.out.println("End first loop");
-        System.out.println("Start second loop");
-        for (int i = 100; i <=200; i = i + 10){
-            System.out.println(i);
-            if (i == 140){
-                for (int b = 150; b <= 159; b++){
-                    System.out.println(b);
-                }
-            }
-        }   
-        System.out.println("End second loop");  
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter your first name. ");
+        String firstName = input.nextLine();
+
+        System.out.println("Please enter your last name. ");
+        String lastName = input.nextLine();
+
+        System.out.println("Please enter your age. ");
+        String age = input.nextLine();
+
+        System.out.println("What grade are you in? ");
+        String grade = input.nextLine();
+
+        System.out.println("What is your favorite color? ");
+        String favColor = input.nextLine();
+        
+
+
+        System.out.println("Hello " + firstName + " " + lastName);
+        System.out.println("Age = " + age);
+        System.out.println("Grade = " + grade);
+        System.out.println("Favorite color is, " + favColor);
     }
 }
