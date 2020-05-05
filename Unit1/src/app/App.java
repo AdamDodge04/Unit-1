a1517b060acaad44efb6e768f5140f4e335eb025
AdamDodge04 <815495@mystma.org>
2020-05-05 16:10:04 +0000

    3 Question input

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,11 +6,32 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner input = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+        Scanner input3 = new Scanner(System.in);
 
-        System.out.println(">What is the count?");
-        int count = input.nextInt();
-        for (int n = 1; n <= count; n++){
-            System.out.println(">" + n);
+        System.out.println("How many inches are in a foot?");
+        int inches = input.nextInt();
+        if (inches == 12){
+            System.out.println("CORRECT");
+        }
+        else {
+            System.out.println("INCORRECT");
+        }
+        System.out.println("What is the capitol of Minnesota?");
+        String answer = input2.nextLine();
+        if (answer.equals("Saint Paul")){
+            System.out.println("CORRECT");
+        }
+        else {
+            System.out.println("INCORRECT");
+        }
+        System.out.println("What year is it?");
+        int year = input3.nextInt();
+        if (year == 2020){
+            System.out.println("CORRECT");
+        }
+        else{
+            System.out.println("INCORRECT");
         }
     }
 }
