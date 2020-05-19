cfc338575f37ab7e95346c20f66256219ca5315e
AdamDodge04 <815495@mystma.org>
2020-05-19 16:31:35 +0000

    Dogs with 6 variables

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -16,30 +16,49 @@ public class App {
         dog1.name = "Beau";
         dog1.breed = "British Labrador";
         dog1.age = 6;
+        dog1.favFood = "Pork";
+        dog1.height = 22;
+        dog1.favActivity = "Running";
 
         dog2.name = "Zeus";
         dog2.breed = "German Shepherd";
         dog2.age = 3;
+        dog2.favFood = "Ice Cream";
+        dog2.height = 24;
+        dog2.favActivity = "Fetch";
+
 
         dog3.name = "Geno";
         dog3.breed = "Shiba Inu";
         dog3.age = 1;
+        dog3.favFood = "Pizza";
+        dog3.height = 18;
+        dog3.favActivity = "Walks";
 
 
         System.out.println("Dog 1");
         System.out.println("name: " + dog1.name);
         System.out.println("breed: " + dog1.breed);
         System.out.println("age: " + dog1.age);
+        System.out.println("Favorite Food: " + dog1.favFood);
+        System.out.println("Height (in): " + dog1.height);
+        System.out.println("Favorite Activity: " + dog1.favActivity);
 
         System.out.println("Dog 2");
         System.out.println("name: " + dog2.name);
         System.out.println("breed: " + dog2.breed);
         System.out.println("age: " + dog2.age);
+        System.out.println("Favorite Food: " + dog2.favFood);
+        System.out.println("Height (in): " + dog2.height);
+        System.out.println("Favorite Activity: " + dog2.favActivity);
         
         System.out.println("Dog 3");
         System.out.println("name: " + dog3.name);
         System.out.println("breed: " + dog3.breed);
         System.out.println("age: " + dog3.age);
+        System.out.println("Favorite Food: " + dog3.favFood);
+        System.out.println("Height (in): " + dog3.height);
+        System.out.println("Favorite Activity: " + dog3.favActivity);
 
     }
 }
--- Unit1/src/app/Dog.java
+++ Unit1/src/app/Dog.java
@@ -5,5 +5,9 @@ public class Dog{
     String name;
     String breed;
     int age;
+    String favFood;
+    int height;
+    String favActivity;
+
 
 }
